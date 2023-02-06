17a3076a6660bd686085a7fd3b57bba01e3d7f56
Morishima Sora <38559874+ForestIsland9014@users.noreply.github.com>
2023-02-06 17:42:22 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,5 +18,32 @@ function hover(obj) {
 }
 
 
+function get_bulletin_board() {
+    var api_url = "https://script.google.com/macros/s/AKfycbz5q6gf-tvuEM75cW5utIjFXmsxhvrkOOtFJNXu8lZ4UK4fYBle3Px3rdN3IGmdizY/exec";
+    fetch(api_url).then(function (fetch_data) {
+        return fetch_data.json();
+    }).then(function (json) {
+        for (var i in json) {
+            let div_tag = document.createElement("div");
+            div_tag.className = "message_block";
+            let h_tag = document.createElement("h3");
+            h_tag.textContent = (parseInt(i, 10)+1) + ". " + json[i].name;
+            let p_tag = document.createElement("p");
+            p_tag.textContent = json[i].message;
+
+            div_tag.appendChild(h_tag);
+            div_tag.appendChild(p_tag);
+            
+            document.getElementById("bulletin_board").appendChild(div_tag);
+            
+
+            console.log(json[i].name, json[i].message);
+        }
+        document.getElementById("loading_now").style.display = "none";
+    });
+
+    
+}
+
 
 
