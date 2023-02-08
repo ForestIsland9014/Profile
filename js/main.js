ad2d4c044c0932bb349af1f4a6bbaf2cbee9e051
Morishima Sora <38559874+ForestIsland9014@users.noreply.github.com>
2023-02-08 15:01:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ function hover(obj) {
     obj.style.transform = "scale(1.02, 1.02)";
     obj.addEventListener('mouseleave', event => {
         obj.style = style;
+        obj.style.opacity = 1;
+        obj.style.transform = 'translateY(0)';
     });
 }
 
@@ -26,6 +28,7 @@ function get_bulletin_board() {
         for (var i in json) {
             let div_tag = document.createElement("div");
             div_tag.className = "message_block";
+            div_tag.id = "message_" + (parseInt(i, 10)+1);
             let h_tag = document.createElement("h3");
             h_tag.textContent = (parseInt(i, 10)+1) + ". " + json[i].name;
             let p_tag = document.createElement("p");
@@ -39,9 +42,74 @@ function get_bulletin_board() {
         }
         document.getElementById("loading_now").style.display = "none";
     });
-
-    
 }
+function wait(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+// 名前を動かす
+$(async function(){
+    $('#profile_name').css({
+        opacity: 0,
+        transform: 'translateY(20px)',
+    });
+    await wait(100);
+    $('#profile_name').css({
+        opacity: 1,
+        transform: 'translateY(0)', 
+        transition: '1200ms'
+    });
+
+});
+$(function(){
+    var scroll_btm = 0;
+    function show() {
+        var scroll_top = $(this).scrollTop();
+        scroll_btm = scroll_top + $(this).height();
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.border').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( scroll_btm-100 > this_pos) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+        $('.headline').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( scroll_btm-100 > this_pos) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateX(0)'
+                });
+            }
+        });
+        $('.portfolio_explanation_block').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( scroll_btm > this_pos) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+        $('.portfolio_link').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( scroll_btm > this_pos) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+    }
+    show();
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        show();
+    });
+});
 
 
 
